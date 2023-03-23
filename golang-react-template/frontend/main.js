b9dd1b39f22c0d322615d30eb325d204dee37a87
rudolfs.zvejs <rudolfs.zvejs@loadero.com>
2023-03-23 11:23:24 +0200

    react: Try react

--- golang-react-template/frontend/main.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  // React
-  var React = require('react');
-  var ReactDOM = require('react-dom');
-
-  var Hello = require("./components/hello");
-  ReactDOM.render(<Hello />, document.getElementById('app'));
-
-})();
