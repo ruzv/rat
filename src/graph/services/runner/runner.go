470159e67ecb64c6f896ea8d3b20d96b5034bc90
ruzv <zvejs.rudolfs@gmail.com>
2024-10-04 10:50:19 +0300

    rat-0018: Fix lint

--- src/graph/services/runner/runner.go
+++ src/graph/services/runner/runner.go
@@ -44,9 +44,9 @@ type Runner struct {
 }
 
 // New creates a new graph services.
-func New(
-	c *Config, webStaticContent fs.FS,
-) (*Runner, *logr.LogR, error) {
+//
+//nolint:gocyclop,cyclop
+func New(c *Config, webStaticContent fs.FS) (*Runner, *logr.LogR, error) {
 	ratLog := logr.NewLogR(os.Stdout, "rat", c.Log)
 
 	ratLog.Infof("%s\nversion: %s", logo, buildinfo.Version())
@@ -62,7 +62,7 @@ func New(
 	runnerServices := []services.Service{}
 
 	if c.Sync != nil {
-		syncer, err := sync.NewSyncer(c.Sync, log) //nolint:govet // shadows err
+		syncer, err := sync.NewSyncer(c.Sync, log) 
 		if err != nil {
 			return nil, nil, errors.Wrap(err, "failed to create syncer")
 		}
